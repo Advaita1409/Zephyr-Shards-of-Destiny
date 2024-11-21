1b92ea7e612007a88e71f09c1022aee7ed60c832
Advaita Sharma <advaitasharma2003@gmail.com>
2024-11-21 21:36:48 +0530

    edited the scene with a camera to follow the player

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     public Transform player;
+    public float smoothNess = 2f;
     // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate(){
+        Vector3 targetPosition = new Vector3(player.position.x,transform.position.y,transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, targetPosition, smoothNess*Time.deltaTime) ;
     }
 }
