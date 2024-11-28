180d28ae5d3c525a8153adc97d33ae4b76eb2b09
Advaita Sharma <advaitasharma2003@gmail.com>
2024-11-28 20:18:13 +0530

    added groundspawner script

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    public GameObject groundPrefab;
+    private Vector2 spawnPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnGround(){
+        //Instantiate( )
+    }
+}
