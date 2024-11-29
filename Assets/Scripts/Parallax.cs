8066e0a73dcf98cc3dedfdbf82bd795e7b10c94c
Advaita Sharma <advaitasharma2003@gmail.com>
2024-11-29 22:30:16 +0530

    script and parallax execution

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public GameOject cam;
+    public GameObject cam;
     public float parallaxEffect;
     private float startPos;
     private float length;
@@ -10,12 +10,14 @@ public class Parallax : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPos = this.transform.position.x;
+        length = this.GetComponent<SpriteRenderer>().size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = cam.transform.position.x * parallaxEffect;
+        transform.position = new Vector3(startPos + distance,0f,0f);
     }
 }
