3fbb10b89b085e127a08991d3e0e72caac29709e
Advaita Sharma <advaitasharma2003@gmail.com>
2024-11-29 23:04:26 +0530

    game tech

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -11,7 +11,7 @@ public class Parallax : MonoBehaviour
     void Start()
     {
         startPos = this.transform.position.x;
-        length = this.GetComponent<SpriteRenderer>().size.x;
+        length = this.GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -19,5 +19,9 @@ public class Parallax : MonoBehaviour
     {
         float distance = cam.transform.position.x * parallaxEffect;
         transform.position = new Vector3(startPos + distance,0f,0f);
+        float temp = cam.transform.position.x + (1-parallaxEffect);
+        if(temp > startPos + length){
+            startPos += length;
+        }
     }
 }
