ef737e3f3c14d375f32628d8e31a6ba3363767a2
Advaita Sharma <advaitasharma2003@gmail.com>
2024-11-29 22:15:22 +0530

    parallax code edited

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameOject cam;
+    public float parallaxEffect;
+    private float startPos;
+    private float length;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
