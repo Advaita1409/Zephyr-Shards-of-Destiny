5bb0af430bb4bd4585ec1edc0c628ac8c4a20fbf
Advaita Sharma <advaitasharma2003@gmail.com>
2024-12-02 02:06:28 +0530

    Menu Scene

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -6,5 +6,9 @@ public class GameOverMenu : MonoBehaviour
     public void RetryButton(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void MenuButton(){
+        SceneManager.LoadScene("Menu");
+    }
 }
 
---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,12 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void PlayButton(){
+        SceneManagement.LoadScene("SampleScene");
+    }
+    public void ExitButton(){
+
+    }
+}
